cdc6fa4d4c7b062b079cf838513ddf01f3acf14c
Deepanshu Sharma <deepanshu.sharma@4a-consulting.com>
2026-02-06 11:33:13 -0500

    Fixed Routing in App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,32 @@
 import { useState } from "react"
 import { Sidebar } from "./components/ui/Sidebar"
- 
+import { Dashboard } from "./components/Dashboard"
 function App() {
   const [activeTab, setActiveTab] = useState("dashboard")
  
   const handleLogout = () => {
     console.log("Logging out...")
   }
+  const renderContent = () => {
+    switch (activeTab) {
+      case 'dashboard':
+        return <Dashboard onNavigate={setActiveTab} />;
+      case 'clients':
+        // return <EmployeeManagement />;
+      case 'invoices':
+        // return <UserAssignment />;
+      case 'analytics':
+        // return <Analytics />;
+      default:
+        return <Dashboard />;
+    }
+  };
  
-  return (
-    <Sidebar activeTab={activeTab} setActiveTab={setActiveTab} onLogout={handleLogout}>
-     
-     <h1 className="bg-amber-50">AR Tracking APP</h1>
-    </Sidebar>
+  return (  
+    <div className="flex h-screen bg-background">
+      <Sidebar activeTab={activeTab} setActiveTab={setActiveTab} onLogout={handleLogout} />
+      <main className="flex-1 overflow-auto">{renderContent()}</main>
+    </div>
   )
 }
  
---
+++ Frontend/src/components/Dashboard.jsx
@@ -0,0 +1,12 @@
+export function Dashboard(){
+    return (
+        <div className="p-6 space-y-6">
+            {/* {Header} */}
+            <div className="flex items-center justify-between">
+                <h1>Dashboard</h1>
+                <p>Welconme Back! Here's What's Happening today</p>
+            </div>
+            
+        </div>
+    )
+}

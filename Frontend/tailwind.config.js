850c1964ac932095751c792fe9eb364752951a90
Hemanth-Theegala <HemanthReddy.Theegala@4a-consulting.com>
2026-02-06 10:28:34 -0500

    Installed Required Dependencies

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+      },
+    },
+  },
+  plugins: [],
+}

0ad885d33e1a8c8d64c42dd001e46e324c693b6a
Hemanth-Theegala <HemanthReddy.Theegala@4a-consulting.com>
2026-02-06 10:39:34 -0500

    configuration changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,20 @@
-
-
+import { useState } from "react"
+import { Sidebar } from "./components/ui/Sidebar"
+ 
 function App() {
-
+  const [activeTab, setActiveTab] = useState("dashboard")
+ 
+  const handleLogout = () => {
+    console.log("Logging out...")
+  }
+ 
   return (
-    <>
-     <h1 className="bg-amber-50">Prestigious 4A Employee - test</h1>
-    </>
+    <Sidebar activeTab={activeTab} setActiveTab={setActiveTab} onLogout={handleLogout}>
+     
+     <h1 className="bg-amber-50">AR Tracking APP</h1>
+    </Sidebar>
   )
 }
-
+ 
 export default App
+ 
---
+++ Frontend/src/imports/one.jsx
@@ -0,0 +1,17 @@
+
+ 
+import imgGeminiGeneratedImage3Mxxuw3Mxxuw3Mxx1 from "figma:asset/7985624beac67b33c1bbccc2fb2c75a0aa15c9b8.png";
+ 
+export default function Frame() {
+  return (
+    <div className="relative size-full">
+      <div className="absolute h-[40px] left-0 top-0 w-[40px]" data-name="Gemini_Generated_Image_3mxxuw3mxxuw3mxx 1">
+        <div className="absolute inset-0 overflow-hidden pointer-events-none">
+          <img alt="" className="absolute h-[268.88%] left-[-152.94%] max-w-none top-[-85.79%] w-[400.98%]" src={imgGeminiGeneratedImage3Mxxuw3Mxxuw3Mxx1} />
+        </div>
+      </div>
+    </div>
+  );
+}
+ 
+ 
